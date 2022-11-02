df4f8eb0439f28b01ef30676944ac7b86a71b18d
Ian Stewart <istewart@kollective.com>
2022-11-01 17:13:04 -0700

    Initial support for Single and Completable (#7)

---
+++ opentelemetry-rxjava3-tracer/src/main/java/io/github/ikstewa/opentelemetry/rxjava3/RxTracingAssembly.java
@@ -0,0 +1,104 @@
+//
+// Copyright 2022 Ian Stewart
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package io.github.ikstewa.opentelemetry.rxjava3;
+
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.internal.GuardedBy;
+import io.opentelemetry.instrumentation.rxjava.v3_1_1.TracingAssembly;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
+import javax.annotation.Nullable;
+
+public final class RxTracingAssembly {
+
+  @GuardedBy("RxTracer.class")
+  @Nullable
+  private static Function<? super Runnable, ? extends Runnable> oldScheduleHandler;
+
+  @GuardedBy("RxTracer.class")
+  private static boolean enabled;
+
+  public static RxTracingAssemblyBuilder builder() {
+    return new RxTracingAssemblyBuilder();
+  }
+
+  private final boolean enableSchedulerPropagation;
+
+  RxTracingAssembly(boolean enableSchedulerPropagation) {
+    this.enableSchedulerPropagation = enableSchedulerPropagation;
+  }
+
+  public RxTracingAssembly enable() {
+    synchronized (RxTracingAssembly.class) {
+      if (enabled) {
+        return this;
+      }
+
+      TracingAssembly.builder()
+          // Adds an attribute when span is cancelled via dispose
+          .setCaptureExperimentalSpanAttributes(true)
+          .build()
+          .enable();
+
+      // Pass context on to other threads
+      oldScheduleHandler = RxJavaPlugins.getScheduleHandler();
+      if (enableSchedulerPropagation) {
+        RxJavaPlugins.setScheduleHandler(
+            compose(
+                oldScheduleHandler,
+                runnable -> {
+                  final var context = Context.current();
+                  return () -> {
+                    try (var s = context.makeCurrent()) {
+                      runnable.run();
+                    }
+                  };
+                }));
+      }
+
+      enabled = true;
+    }
+    return this;
+  }
+
+  public RxTracingAssembly disable() {
+    synchronized (RxTracingAssembly.class) {
+      if (!enabled) {
+        return this;
+      }
+
+      // FIXME: This could potentially remove TracingAssembly if initialized separately
+      TracingAssembly.create().disable();
+
+      RxJavaPlugins.setScheduleHandler(oldScheduleHandler);
+      oldScheduleHandler = null;
+
+      enabled = false;
+    }
+    return this;
+  }
+
+  // --------------------------------------------------------------------------------
+  // Helper methods
+
+  private static <T> Function<? super T, ? extends T> compose(
+      Function<? super T, ? extends T> before, Function<? super T, ? extends T> after) {
+    if (before == null) {
+      return after;
+    }
+    return (T v) -> after.apply(before.apply(v));
+  }
+}
---
+++ opentelemetry-rxjava3-tracer/src/main/java/io/github/ikstewa/opentelemetry/rxjava3/TracedCompletable.java
@@ -0,0 +1,90 @@
+//
+// Copyright 2022 Ian Stewart
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package io.github.ikstewa.opentelemetry.rxjava3;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.core.CompletableSource;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.internal.disposables.DisposableHelper;
+import java.util.concurrent.atomic.AtomicReference;
+
+class TracedCompletable extends Completable {
+
+  private final CompletableSource source;
+  private final SpanBuilder spanBuilder;
+
+  public TracedCompletable(CompletableSource source, SpanBuilder span) {
+    this.source = source;
+    this.spanBuilder = span;
+  }
+
+  @Override
+  protected void subscribeActual(@NonNull CompletableObserver observer) {
+    final var span = spanBuilder.startSpan();
+    try (var ignored = span.makeCurrent()) {
+      source.subscribe(new TracedCompletableObserver(observer, span));
+    }
+  }
+
+  private static final class TracedCompletableObserver extends AtomicReference<Disposable>
+      implements CompletableObserver, Disposable {
+
+    private final CompletableObserver actualObserver;
+    private final Span span;
+
+    public TracedCompletableObserver(CompletableObserver observer, Span span) {
+      this.actualObserver = observer;
+      this.span = span;
+    }
+
+    @Override
+    public void onSubscribe(Disposable d) {
+      if (DisposableHelper.setOnce(this, d)) {
+        actualObserver.onSubscribe(this);
+      }
+    }
+
+    @Override
+    public void onComplete() {
+      RxTracer.spanComplete(span);
+
+      actualObserver.onComplete();
+    }
+
+    @Override
+    public void onError(Throwable e) {
+      RxTracer.spanError(span, e);
+
+      actualObserver.onError(e);
+    }
+
+    @Override
+    public void dispose() {
+      DisposableHelper.dispose(this);
+
+      RxTracer.spanDispose(span);
+    }
+
+    @Override
+    public boolean isDisposed() {
+      return DisposableHelper.isDisposed(get());
+    }
+  }
+}
